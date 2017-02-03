15bad24f955c8a1ebee0ff1e71f3971cffc4ef1b
fllalpha <fllalpha@10.100.8.130>
2017-02-03 15:24:02 -0500

    gyroRestart. BAM 2 sec - Harrison, Kieran

---
+++ src/org/usfirst/frc5902/robot/commands/gyroRestart.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc5902.robot.commands;
+
+import org.usfirst.frc5902.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class gyroRestart extends Command {
+	public static double angle;
+    public gyroRestart() {
+    	requires(Robot.gyro);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	angle = 0;
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	angle = Robot.gyro.angle;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
